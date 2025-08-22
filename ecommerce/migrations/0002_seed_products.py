4a8ac0cb35a5a0b9309ed2c3f6a852ffd9b50dc4
StarvingDogs <splaylime@gmail.com>
2025-08-22 11:39:59 +0100

    add faker

--- ecommerce/migrations/0002_seed_products.py
+++ ecommerce/migrations/0002_seed_products.py
@@ -1,18 +1,20 @@
 from django.db import migrations
-
-import random
+from faker import Faker
 
 def seed_products(apps, schema_editor):
     Product = apps.get_model('ecommerce', 'Product')
+    fake = Faker()
+    categories = [fake.word() for _ in range(5)]
+    brands = [fake.company() for _ in range(10)]
     products = []
     for i in range(1, 101):
-        price = round(random.uniform(5.0, 500.0), 2)
-        stock = random.randint(1, 200)
+        price = round(fake.pyfloat(left_digits=3, right_digits=2, positive=True, min_value=5.0, max_value=500.0), 2)
+        stock = fake.random_int(min=1, max=200)
         products.append(Product(
-            name=f'Product {i}',
-            brand=f'Brand {i % 10}',
-            category=f'Category {i % 5}',
-            description=f'Sample description for product {i}.',
+            name=fake.unique.word().title() + f" {i}",
+            brand=brands[i % 10],
+            category=categories[i % 5],
+            description=fake.text(max_nb_chars=100),
             price=price,
             stock=stock,
             image=''  # Leave blank for now
